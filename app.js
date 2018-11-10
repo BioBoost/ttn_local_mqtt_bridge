8833635c17b2dece6bc4e2cfc014e588758b7f2a
Nico De Witte <nico.dewitte@outlook.com>
2018-11-10 19:22:53 +0100

    Read the settings file

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+const fs = require('fs');
+
 console.log("Launching The Things Network Local MQTT Bridge");
+
+let config = null;
+try {
+  config = JSON.parse(fs.readFileSync('settings.json', 'utf8'));
+} catch (e) {
+  console.log("Could not load settings.json file. Is it there?");
+  process.exit();
+}
