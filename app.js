0757ba578ae764fbd02f2158a91ebc421f2e1b9a
Nico De Witte <nico.dewitte@outlook.com>
2018-11-10 19:28:52 +0100

    Create bridge object for each entry in config

--- app.js
+++ app.js
@@ -9,3 +9,21 @@ try {
   console.log("Could not load settings.json file. Is it there?");
   process.exit();
 }
+
+bridges = [];
+config.bridges.forEach(function(item, index, array) {
+  console.log("Creating bridge");
+
+  let bridge = {
+    local: config.local.connection,
+    remote: config.remote.connection,
+    username: item.remote.app_id,
+    password: item.remote.base64_access_key,
+    devices: item.remote.devices,
+    local_topic: item.local.topic
+  };
+
+  bridges.push(bridge);
+});
+
+console.log(bridges);
